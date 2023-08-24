c3b97ec091ad555c531a727166200b022ad72075
accompany1205 <accompaniedwithyou@gmail.com>
2023-08-24 01:02:52 -0700

    aaa

--- src/App.js
+++ src/App.js
@@ -8,31 +8,6 @@ const BOT_TOKEN = '6517591713:AAG6eQwp00UOKUurahUMeh4ahxvIwKmYRYI';
 const USERNAME_TO_CHECK = 'accompany1205';
 
 const App = () => {
-
-  useEffect(() => {
-    checkUsernameValidity();
-  }, [])
-
-  async function checkUsernameValidity() {
-    try {
-      const response = await axios.get(`https://api.telegram.org/bot${BOT_TOKEN}/getChat`, {
-        params: {
-          chat_id: `@${USERNAME_TO_CHECK}`
-      }
-      });
-
-      console.log(response.status);
-
-      if (response.status === 200) {
-        console.log(`Username @${USERNAME_TO_CHECK} is valid`);
-      } else {
-        console.log(`Username @${USERNAME_TO_CHECK} is not valid`);
-      }
-    } catch (error) {
-      console.error('Error checking username:', error.message);
-    }
-  }
-
   return (
     <>
       
